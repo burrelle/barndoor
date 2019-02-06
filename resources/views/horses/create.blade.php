8ac45262d03320ef77bfcb83b6283d29e83dc3b0
Evan Burrell <burrelle@oregonstate.edu>
2019-02-06 21:20:47 +0000

    Styling create.blade.php

--- resources/views/horses/create.blade.php
+++ resources/views/horses/create.blade.php
@@ -1,28 +1,111 @@
-<form method="POST" action="/horses" enctype="multipart/form-data">
-    @csrf
-    <label for="name">
-        Name
-        <input type="text" name="name" id="name" />
-    </label>
-    <label for="gender">
-        Gender
-        <select name="gender" id="gender">
-            <option value="mare">Mare</option>
-            <option value="gelding">Gelding</option>
-            <option value="stallion">Stallion</option>
-        </select>
-    </label>
-    <label for="breed">
-        Breed
-        <input type="text" name="breed" id="breed" />
-    </label>
-    <label for="color">
-        Color
-        <input type="text" name="color" id="color" />
-    </label>
-    <label for="height">
-        Height
-        <input type="text" name="height" id="height" /> hh
-    </label>
-    <button type="submit">Submit</button>
-</form>
+@extends('layouts.app') @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add Horse</div>
+                <div class="card-body">
+                    <form
+                        method="POST"
+                        action="/horses"
+                        enctype="multipart/form-data"
+                    >
+                        @csrf
+                        <div class="form-group row">
+                            <label
+                                class="col-md-4 col-form-label text-md-right"
+                                for="name"
+                            >
+                                Name
+                            </label>
+                            <div class="col-md-6">
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label
+                                class="col-md-4 col-form-label text-md-right"
+                                for="gender"
+                            >
+                                Gender
+                            </label>
+                            <div class="col-md-6">
+                                <select
+                                    class="form-control"
+                                    name="gender"
+                                    id="gender"
+                                >
+                                    <option value="mare">Mare</option>
+                                    <option value="gelding">Gelding</option>
+                                    <option value="stallion">Stallion</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label
+                                class="col-md-4 col-form-label text-md-right"
+                                for="breed"
+                            >
+                                Breed
+                            </label>
+                            <div class="col-md-6">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="breed"
+                                    id="breed"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label
+                                class="col-md-4 col-form-label text-md-right"
+                                for="color"
+                            >
+                                Color
+                            </label>
+                            <div class="col-md-6">
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    name="color"
+                                    id="color"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label
+                                class="col-md-4 col-form-label text-md-right"
+                                for="height"
+                            >
+                                Height
+                            </label>
+                            <div class="col-md-6">
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    name="height"
+                                    id="height"
+                                />
+                            </div>
+                            hh
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
